2c94537a439f4655dfc1fdc794aece8b98a6d685
apexSnehith <snehith0713@gmail.com>
2023-05-26 13:35:08 +0530

    frontend content

--- frontend.sh
+++ frontend.sh
@@ -1,38 +1,15 @@
-#installing nginx
+echo -e "\e[32minstalling nginx \e[0m"
 yum install nginx -y
-#start&enable nginx
-systemctl enable nginx
-systemctl start nginx
-#remove default nginx content on the web
+echo -e "\e[32mremove default nginx content on the web\e[0m"
 cd /usr/share/nginx/html/ ; pwd ; ls -ls
 cd
 rm -rf /usr/share/nginx/html/*
-#download the frontend content.
+echo -e "\e[32mdownload the frontend content.\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-#extract the fronted content
+echo -e "\e[32extract the fronted content.\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-
-#create nginx reverse proxy configuration file
-
-#Add the following content
- #/etc/nginx/default.d/roboshop.conf
- #
- #proxy_http_version 1.1;
- #location /images/ {
- #  expires 5s;
- #  root   /usr/share/nginx/html;
- #  try_files $uri /images/placeholder.jpg;
- #}
- #location /api/catalogue/ { proxy_pass http://localhost:8080/; }
- #location /api/user/ { proxy_pass http://localhost:8080/; }
- #location /api/cart/ { proxy_pass http://localhost:8080/; }
- #location /api/shipping/ { proxy_pass http://localhost:8080/; }
- #location /api/payment/ { proxy_pass http://localhost:8080/; }
- #
- #location /health {
- #  stub_status on;
- #  access_log off;
- #}
-
- systemctl restart nginx
+#create nginx reverse proxy configuration file / we need to copy config file.
+echo -e "\e[32mstarting&enabling nginx\e[0m"
+systemctl enable nginx
+systemctl restart nginx
