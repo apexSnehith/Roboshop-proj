a8817b94a82ae2750c6dfdb1067f0d6d2a8bda1c
apexSnehith <snehith0713@gmail.com>
2023-05-26 14:21:44 +0530

    Mongodb content

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,5 @@
 echo -e "\e[32m Copy Mongodb Repo file \e[0m" $>> /tmp/mongodb.log
-cp mongodb.repo \etc/yum.repos.d/momgo.repo
+cp mongodb.repo /etc/yum.repos.d/mongo.repo
 echo -e "\e[32m Installing Mongodb \e[0m" &>> /tmp/mongodb.log
 yum install mongodb-org -yum
 #Modify the Config file to set the "localhost(127.0.0.1) to localhost(0.0.0.0) in the /etc/mongodb.conf
