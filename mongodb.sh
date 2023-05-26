afabf24889ca9c57088c52363b49989e0a457ee6
apexSnehith <snehith0713@gmail.com>
2023-05-26 14:30:01 +0530

    Mongodb content

--- mongodb.sh
+++ mongodb.sh
@@ -5,6 +5,7 @@ echo -e "\e[32m Installing Mongodb \e[0m"
 yum install mongodb-org -y &>>/tmp/mongodb.log
 
 #Modify the Config file to set the "localhost(127.0.0.1) to localhost(0.0.0.0) in the /etc/mongodb.conf
-echo -e "\e[32m Restaring the Mongodb service \e[0m" &>>/tmp/momgodb.log
+
+echo -e "\e[32m Restaring the Mongodb service \e[0m"
 systemctl enable mongod &>>/tmp/mongodb.log
 systemctl restart mongod &>>/tmp/mongodb.log
