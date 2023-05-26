abc896cb915989240154014f64c395e9b95e88a3
apexSnehith <snehith0713@gmail.com>
2023-05-26 14:28:22 +0530

    Mongodb content

--- mongodb.sh
+++ mongodb.sh
@@ -1,10 +1,10 @@
-echo -e "\e[32m Copy Mongodb Repo file \e[0m" &>>/tmp/mongodb.log
-cp mongodb.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[32m Copy Mongodb Repo file \e[0m"
+cp mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/mongodb.log
 
-echo -e "\e[32m Installing Mongodb \e[0m" &>>/tmp/mongodb.log
-yum install mongodb-org -y
+echo -e "\e[32m Installing Mongodb \e[0m"
+yum install mongodb-org -y &>>/tmp/mongodb.log
 
 #Modify the Config file to set the "localhost(127.0.0.1) to localhost(0.0.0.0) in the /etc/mongodb.conf
 echo -e "\e[32m Restaring the Mongodb service \e[0m" &>>/tmp/momgodb.log
-systemctl enable mongod
-systemctl restart mongod
+systemctl enable mongod &>>/tmp/mongodb.log
+systemctl restart mongod &>>/tmp/mongodb.log
