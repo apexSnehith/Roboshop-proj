53148307128d9fc7a191a151e0d18d601eb0bb1d
apexSnehith <snehith0713@gmail.com>
2023-05-26 13:41:19 +0530

    frontend content

--- frontend.sh
+++ frontend.sh
@@ -1,15 +1,17 @@
-echo -e "\e[32minstalling nginx \e[0m"
+echo -e "\e[32m Installing nginx \e[0m"
 yum install nginx -y
-echo -e "\e[32mremove default nginx content on the web\e[0m"
-cd /usr/share/nginx/html/ ; pwd ; ls -ls
-cd
+
+echo -e "\e[32m Removing default nginx content on the web\e[0m"
 rm -rf /usr/share/nginx/html/*
-echo -e "\e[32mdownload the frontend content.\e[0m"
+
+echo -e "\e[32m Downloading the frontend content.\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-echo -e "\e[32extract the fronted content.\e[0m"
+
+echo -e "\e[32 Extracting the fronted content.\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+
 #create nginx reverse proxy configuration file / we need to copy config file.
-echo -e "\e[32mstarting&enabling nginx\e[0m"
+echo -e "\e[32m Starting&enabling nginx server\e[0m"
 systemctl enable nginx
 systemctl restart nginx
