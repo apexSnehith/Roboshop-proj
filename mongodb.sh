87a16bc2f237985010908bdeccf63c96e7d3609c
apexSnehith <snehith0713@gmail.com>
2023-05-30 08:04:12 +0530

    Configuring the localhost for mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -5,8 +5,9 @@ echo -e "\e[32m Installing Mongodb \e[0m"
 yum install roboshop-org -y &>>/tmp/roboshop.log
 
 #Modify the Config file to set the "localhost(127.0.0.1) to localhost(0.0.0.0) in the /etc/roboshop.conf
-echo -e "\e[32m Configuring the localhost \e[0m"
+echo -e "\e[32m Configuring the Mongodb listen address \e[0m"
 sed -i "s127.0.0.1/0.0.0.0/" /etc/mongod.conf
+
 echo -e "\e[32m Restaring the Mongodb service \e[0m"
 systemctl enable mongod &>>/tmp/roboshop.log
 systemctl restart mongod &>>/tmp/roboshop.log
