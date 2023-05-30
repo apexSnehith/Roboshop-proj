1a434759394df3317c22530af550a4ebd960e05c
apexSnehith <snehith0713@gmail.com>
2023-05-30 22:38:23 +0530

    setting up the mysql

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,7 @@
 echo -e "\e[32mDisblin the previous sql version\e[0m"
 yum module disable mysql -y
 echo -e "\e[32mSetting up the mysql repo\e[0m"
-cp /home/centos/Roboshop-proj/mysql.repo /et/yum.repos.d/mysql.repo
+cp /home/centos/Roboshop-proj/mysql.repo /etc/yum.repos.d/mysql.repo
 echo -e "\e[32minstalling the MySQL Server\e[0m"
 yum install mysql-community-server -y
 echo -e "\e[32mStarting&Enabling the service\e[0m"
