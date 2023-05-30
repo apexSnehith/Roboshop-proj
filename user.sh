e025304d7c0b26902a57e20a5ddd5b9dbcae7379
apexSnehith <snehith0713@gmail.com>
2023-05-30 22:08:21 +0530

    Setting up the redis

--- user.sh
+++ user.sh
@@ -1,28 +1,30 @@
-echo -e "\e[32mSetting up the NodeJS repos\e[0m"
+echo -e "\e[32msetting up the Nodejs repos \e[0m"
+#this downloading a file
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
-echo -e "\e[32mInstalling NodeJS\e[0m"
+echo -e "\e[32minstalling the nodejs\e[0m"
+#intalling
 yum install nodejs -y &>>/tmp/roboshop.log
-echo -e "\e[32mAdding the user for the service\e[0m"
+echo -e "\e[32madding user for the service\e[0m"
 useradd roboshop &>>/tmp/roboshop.log
+echo -e "\e[32mcreate application dir\e[0m"
 rm -rf /app &>>/tmp/roboshop.log
-echo -e "\e[32mmaking a directory \e[0m"
 mkdir /app &>>/tmp/roboshop.log
-echo -e "\e[32mDowloding the application code \e[0m"
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>>/tmp/roboshop.log
+echo -e "\e[32mdownloading the app code to the app dir"
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>>/tmp/roboshop.log
+echo -e "\e[32mUnziping\e[0m"
 cd /app
-echo -e "\e[32mExtracting the file\e[0m"
 unzip /tmp/user.zip &>>/tmp/roboshop.log
-echo -e "\e[32mInstalling the dependencies \e[0m"
+echo -e "\e[32mDownloading the dependencies\e[0m"
 npm install &>>/tmp/roboshop.log
-echo -e "\e[32mSetting up the service \e[0m"
+echo -e "\e[32msetup systemd service\e[0m"
 cp /home/centos/Roboshop-proj/user.service /etc/systemd/system/user.service &>>/tmp/roboshop.log
-echo -e "\e[32mEnabling&restarting the service \e[0m"
+echo -e "\e[32mRestart services\\e[0m"
 systemctl daemon-reload &>>/tmp/roboshop.log
 systemctl enable user &>>/tmp/roboshop.log
 systemctl restart user &>>/tmp/roboshop.log
-echo -e "\e[32mSetting up the schema to install the mongodb client \e[0m"
-cp /home/centos/Roboshop-proj/mongo.repo /etc/systemd/system/mongo.repo &>>/tmp/roboshop.log
-echo -e "\e[32mInstalling the mongodb client \e[0m"
+echo -e "\e[32mInstall Mongodb client\e[0m"
+cp /home/centos/Roboshop-proj/mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 yum install mongodb-org-shell -y &>>/tmp/roboshop.log
-echo -e "\e[32mLoading the Schema \e[0m"
+echo -e "\e[32mload schema\e[0m"
 mongo --host mongodb-dev.snehithdops.online </app/schema/user.js &>>/tmp/roboshop.log
+systemctl restart user >>/tmp/roboshop.log
