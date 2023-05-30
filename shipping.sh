7b187564b7c7daa3ea8d5eff9bb0e5f26b4acf2f
apexSnehith <snehith0713@gmail.com>
2023-05-30 22:52:34 +0530

    setting up the mysql

--- shipping.sh
+++ shipping.sh
@@ -1,25 +1,25 @@
 echo -e "e\[32mInstalling Maven\e[0m"
-yum install maven -yum
+yum install maven -yum &>>/tmp/roboshop.log
 echo -e "e\[32mAdding user for the service\e[0m"
-useradd roboshop
+useradd roboshop &>>/tmp/roboshop.log
 echo -e "e\[32mMaking a directory\e[0m"
-mkdir /app
+mkdir /app &>>/tmp/roboshop.log
 echo -e "e\[32mDownloading the application code\e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip &>>/tmp/roboshop.log
 cd /app
 echo -e "e\[32mExtracting\e[0m"
-unzip /tmp/shipping.zip
+unzip /tmp/shipping.zip &>>/tmp/roboshop.log
 echo -e "e\[32mDownloading the dependencies & Building the application\e[0m"
 cd /app
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
+mvn clean package &>>/tmp/roboshop.log
+mv target/shipping-1.0.jar shipping.jar &>>/tmp/roboshop.log
 echo -e "e\[32mSetting up the Shipping service in the SystemD\e[0m"
-cp /home/centos/Roboshop-proj/shipping.service /etc/systemd/system/shipping.service
+cp /home/centos/Roboshop-proj/shipping.service /etc/systemd/system/shipping.service &>>/tmp/roboshop.log
 echo -e "e\[32mInstalling&Loading the Schema\e[0m"
-yum install mysql -y
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/schema/shipping.sql
+yum install mysql -y &>>/tmp/roboshop.log
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>/tmp/roboshop.log
 echo -e "e\[32mEnabling&restarting the service\e[0m"
-systemctl daemon-reload
-systemctl enable shipping
-systemctl restart shipping
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable shipping &>>/tmp/roboshop.log
+systemctl restart shipping &>>/tmp/roboshop.log
 
