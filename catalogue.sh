61bd8b6a577b6ec7b9e0f0d66f192937362e363f
apexSnehith <snehith0713@gmail.com>
2023-05-30 09:39:53 +0530

    creating the catalogue service.

--- catalogue.sh
+++ catalogue.sh
@@ -5,7 +5,7 @@ yum install nodejs -y &>>/tmp/roboshop.log
 echo -e "\e[32madding user for the service\e[0m"
 useradd roboshop &>>/tmp/roboshop.log
 echo -e "\e[32mcreate application dir\e[0m"
-rm -rf /app &>>/tmp/roboshop.log
+#rm -rf /app &>>/tmp/roboshop.log
 mkdir /app &>>/tmp/roboshop.log
 echo -e "\e[32mdownloading the app code to the app dir"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
@@ -13,6 +13,7 @@ echo -e "\e[32mUnziping\e[0m"
 cd /app
 unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
 echo -e "\e[32mDownloading the dependencies\e[0m"
+cd /app
 npm install &>>/tmp/roboshop.log
 echo -e "\e[32msetup systemd service\e[0m"
 cp /home/centos/Roboshop-proj/catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
