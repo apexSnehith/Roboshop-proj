33fe025903c70f68056a8efa365d8821bb82c9e0
apexSnehith <snehith0713@gmail.com>
2023-05-30 22:39:55 +0530

    setting up the mysql

--- mysql.sh
+++ mysql.sh
@@ -1,12 +1,12 @@
 echo -e "\e[32mDisblin the previous sql version\e[0m"
-yum module disable mysql -y
+yum module disable mysql -y &>> /tmp/roboshop.log
 echo -e "\e[32mSetting up the mysql repo\e[0m"
-cp /home/centos/Roboshop-proj/mysql.repo /etc/yum.repos.d/mysql.repo
+cp /home/centos/Roboshop-proj/mysql.repo /etc/yum.repos.d/mysql.repo &>> /tmp/roboshop.log
 echo -e "\e[32minstalling the MySQL Server\e[0m"
-yum install mysql-community-server -y
+yum install mysql-community-server -y &>> /tmp/roboshop.log
 echo -e "\e[32mStarting&Enabling the service\e[0m"
-systemctl enable mysqld
-systemctl restart mysqld
+systemctl enable mysqld &>> /tmp/roboshop.log
+systemctl restart mysqld &>> /tmp/roboshop.log
 echo -e "\e[32mChanging the default root password\e[0m"
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass RoboShop@1 &>> /tmp/roboshop.log
 #mysql -uroot -pRoboShop@1
