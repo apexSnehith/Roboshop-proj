c157eab39a19a6d04c2fe9af1f493a6255ac456d
apexSnehith <snehith0713@gmail.com>
2023-05-30 21:47:16 +0530

    Setting up the redis

--- user.sh
+++ user.sh
@@ -1,27 +1,28 @@
 echo -e "\e[32mSetting up the NodeJS repos\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
 echo -e "\e[32mInstalling NodeJS\e[0m"
-yum install nodejs -y
+yum install nodejs -y &>>/tmp/roboshop.log
 echo -e "\e[32mAdding the user for the service\e[0m"
-useradd roboshop
+useradd roboshop &>>/tmp/roboshop.log
+rm -rf /app &>>/tmp/roboshop.log
 echo -e "\e[32mmaking a directory \e[0m"
-mkdir /app
+mkdir /app &>>/tmp/roboshop.log
 echo -e "\e[32mDowloding the application code \e[0m"
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>>/tmp/roboshop.log
 cd /app
 echo -e "\e[32mExtracting the file\e[0m"
-unzip /tmp/user.zip
+unzip /tmp/user.zip &>>/tmp/roboshop.log
 echo -e "\e[32mInstalling the dependencies \e[0m"
-npm install
+npm install &>>/tmp/roboshop.log
 echo -e "\e[32mSetting up the service \e[0m"
-cp /home/centos/Roboshop-proj/user.service /etc/systemd/system/user.service
+cp /home/centos/Roboshop-proj/user.service /etc/systemd/system/user.service &>>/tmp/roboshop.log
 echo -e "\e[32mEnabling&restarting the service \e[0m"
-systemctl daemon-reload
-systemctl enabling user
-systemctl restart user
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enabling user &>>/tmp/roboshop.log
+systemctl restart user &>>/tmp/roboshop.log
 echo -e "\e[32mSetting up the schema to install the mongodb client \e[0m"
-cp /home/centos/Roboshop-proj/mongo.repo /etc/systemd/system/mongo.repo
+cp /home/centos/Roboshop-proj/mongo.repo /etc/systemd/system/mongo.repo &>>/tmp/roboshop.log
 echo -e "\e[32mInstalling the mongodb client \e[0m"
-yum install mongodb-org-shell -y
+yum install mongodb-org-shell -y &>>/tmp/roboshop.log
 echo -e "\e[32mLoading the Schema \e[0m"
-mongo --host MONGODB-SERVER-IPADDRESS </app/schema/user.js
+mongo --host MONGODB-SERVER-IPADDRESS </app/schema/user.js &>>/tmp/roboshop.log
