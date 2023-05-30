be64c505a8d3203b5d1bf10d5c9a25c478405c73
apexSnehith <snehith0713@gmail.com>
2023-05-30 21:24:49 +0530

    Setting up the redis

--- redis.sh
+++ redis.sh
@@ -1,11 +1,11 @@
 echo -e "\e[32minstalling redis repo file as a rpm \e[0m"
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>/tmp/roboshop.log
 echo -e "\e[32mEnabling Redis 6.2 from package strean \e[0m"
-yum module enable redis:remi-6.2 -y
+yum module enable redis:remi-6.2 -y &>>/tmp/roboshop.log
 echo -e "\e[32mInstalling redis\e[0m"
-yum install redis -y
+yum install redis -y &>> /tmp/roboshop.log
 echo -e "\e[32mSetting redis listen address\e[0m"
-sed -i -e "s/127.0.0.1/0.0.0.0/" /etc/redis/redis.conf
+sed -i -e "s/127.0.0.1/0.0.0.0/" /etc/redis/redis.conf &>> /tmp/roboshop.log
 echo -e "\e[32mEnabling & Starting the service\e[0m"
-systemctl enable redis
-systemctl restart redis
+systemctl enable redis &>> /tmp/roboshop.log
+systemctl restart redis &>> /tmp/roboshop.log
