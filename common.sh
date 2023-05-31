b71fa2f4da8e18f80b2aa78d9636b4f58a9b324c
apexSnehith <snehith0713@gmail.com>
2023-05-31 23:15:39 +0530

    creating functions

--- common.sh
+++ common.sh
@@ -3,30 +3,45 @@ nocolor="\e[0m"
 log_file="/tmp/roboshop.log"
 app_path="/app"
 
+app_presentup(){
+    echo -e "${color}adding user for the service${nocolor}"
+    useradd roboshop &>>${log_file}
+
+    echo -e "${color}create application dir${nocolor}"
+    rm -rf ${app_path} &>>${log_file}
+    mkdir ${app_path} &>>${log_file}
+    echo -e "${color}downloading the app code to the app dir"
+    curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>${log_file}
+
+    echo -e "${color}Unziping${nocolor}"
+    cd ${app_path}
+    unzip /tmp/$component.zip &>>${log_file}
+}
+
+systemd_setup(){
+
+  echo -e "${color}setup systemd service${nocolor}"
+  cp /home/centos/Roboshop-proj/$component.service /etc/systemd/system/$component.service &>>${log_file}
+
+  echo -e "${color}Enabling&restarting the service${nocolor}"
+  systemctl daemon-reload &>>${log_file}
+  systemctl enable $component &>>${log_file}
+  systemctl restart $component &>>${log_file}
+}
+
 nodejs(){
   echo -e "${color}setting up the Nodejs repos ${nocolor}"
   #this downloading a file
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
   echo -e "${color}installing the nodejs${nocolor}"
   yum install nodejs -y &>>${log_file}
-  echo -e "${color}adding user for the service${nocolor}"
-  useradd roboshop &>>${log_file}
-  echo -e "${color}create application dir${nocolor}"
-  rm -rf ${app_path} &>>${log_file}
-  mkdir ${app_path} &>>${log_file}
-  echo -e "${color}downloading the app code to the app dir"
-  curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>${log_file}
-  echo -e "${color}Unziping${nocolor}"
-  cd ${app_path}
-  unzip /tmp/$component.zip &>>${log_file}
+
+  app_presentup
+
   echo -e "${color}Downloading the dependencies${nocolor}"
   npm install &>>${log_file}
-  echo -e "${color}setup systemd service${nocolor}"
-  cp /home/centos/Roboshop-proj/$component.service /etc/systemd/system/$component.service &>>${log_file}
-  echo -e "${color}Restart services\${nocolor}"
-  systemctl daemon-reload &>>${log_file}
-  systemctl enable $component &>>${log_file}
-  systemctl restart $component &>>${log_file}
+
+  systemd_setup
 }
 
 mongo_schema_setup(){
@@ -36,6 +51,28 @@ mongo_schema_setup(){
  echo -e "${color}load schema${nocolor}"
  mongo --host mongodb-dev.snehithdops.online <${app_path}/schema/$component.js &>>${log_file}
  systemctl restart $component >>${log_file}
+}
+
+mysql_schema_setup(){
+  echo -e "${color}Installing&Loading the Schema${nocolor}"
+  yum install mysql -y >>${log_file}
+  mysql -h mysql-dev.snehithdops.online -uroot -pRoboShop@1 < /app/schema/${component}.sql >>${log_file}
+
+}
+
+maven(){
+  echo -e "${color}Installing Maven${nocolor}"
+  yum install maven -y >>${log_file}
+
+  app_presentup
+
+  echo -e "${color}Downloading the dependencies & Building the application${nocolor}"
+  cd /app
+  mvn clean package >>${log_file}
+  mv target/${component}-1.0.jar ${component}.jar >>${log_file}
+
+  mysql_schema_setup
+  systemd_setup
 }
 
 
--- shipping.sh
+++ shipping.sh
@@ -1,26 +1,5 @@
-echo -e "\e[32mInstalling Maven\e[0m"
-yum install maven -y &>>/tmp/roboshop.log
-echo -e "\e[32mAdding user for the service\e[0m"
-useradd roboshop &>>/tmp/roboshop.log
-echo -e "\e[32mMaking a directory\e[0m"
-rm -rf /app &>>/tmp/roboshop.log
-mkdir /app &>>/tmp/roboshop.log
-echo -e "\e[32mDownloading the application code\e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip &>>/tmp/roboshop.log
-cd /app
-echo -e "\e[32mExtracting\e[0m"
-unzip /tmp/shipping.zip &>>/tmp/roboshop.log
-echo -e "\e[32mDownloading the dependencies & Building the application\e[0m"
-cd /app
-mvn clean package &>>/tmp/roboshop.log
-mv target/shipping-1.0.jar shipping.jar &>>/tmp/roboshop.log
-echo -e "\e[32mSetting up the Shipping service in the SystemD\e[0m"
-cp /home/centos/Roboshop-proj/shipping.service /etc/systemd/system/shipping.service &>>/tmp/roboshop.log
-echo -e "\e[32mInstalling&Loading the Schema\e[0m"
-yum install mysql -y &>>/tmp/roboshop.log
-mysql -h mysql-dev.snehithdops.online -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>/tmp/roboshop.log
-echo -e "\e[32mEnabling&restarting the service\e[0m"
-systemctl daemon-reload &>>/tmp/roboshop.log
-systemctl enable shipping &>>/tmp/roboshop.log
-systemctl restart shipping &>>/tmp/roboshop.log
+source common.sh
+component=shipping
+
+maven
 
