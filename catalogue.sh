9ff2cfaddefaab67a4d8e057e460b2ae458988e4
apexSnehith <snehith0713@gmail.com>
2023-05-31 09:07:04 +0530

    learn shell

--- catalogue.sh
+++ catalogue.sh
@@ -1,31 +1,33 @@
 component=catalogue
-echo -e "\e[32msetting up the Nodejs repos \e[0m"
+color="e[33m"
+nocolor="\e[0m"
+echo -e "${color}setting up the Nodejs repos ${nocolor}"
 #this downloading a file
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
-echo -e "\e[32minstalling the nodejs\e[0m"
+echo -e "${color}installing the nodejs${nocolor}"
 #intalling
 yum install nodejs -y &>>/tmp/roboshop.log
-echo -e "\e[32madding user for the service\e[0m"
+echo -e "${color}adding user for the service${nocolor}"
 useradd roboshop &>>/tmp/roboshop.log
-echo -e "\e[32mcreate application dir\e[0m"
+echo -e "${color}create application dir${nocolor}"
 rm -rf /app &>>/tmp/roboshop.log
 mkdir /app &>>/tmp/roboshop.log
-echo -e "\e[32mdownloading the app code to the app dir"
+echo -e "${color}downloading the app code to the app dir"
 curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>/tmp/roboshop.log
-echo -e "\e[32mUnziping\e[0m"
+echo -e "${color}Unziping${nocolor}"
 cd /app
 unzip /tmp/$component.zip &>>/tmp/roboshop.log
-echo -e "\e[32mDownloading the dependencies\e[0m"
+echo -e "${color}Downloading the dependencies${nocolor}"
 npm install &>>/tmp/roboshop.log
-echo -e "\e[32msetup systemd service\e[0m"
+echo -e "${color}setup systemd service${nocolor}"
 cp /home/centos/Roboshop-proj/$component.service /etc/systemd/system/$component.service &>>/tmp/roboshop.log
-echo -e "\e[32mRestart services\\e[0m"
+echo -e "${color}Restart services\${nocolor}"
 systemctl daemon-reload &>>/tmp/roboshop.log
 systemctl enable $component &>>/tmp/roboshop.log
 systemctl restart $component &>>/tmp/roboshop.log
-echo -e "\e[32mInstall Mongodb client\e[0m"
+echo -e "${color}Install Mongodb client${nocolor}"
 cp /home/centos/Roboshop-proj/mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 yum install mongodb-org-shell -y &>>/tmp/roboshop.log
-echo -e "\e[32mload schema\e[0m"
+echo -e "${color}load schema${nocolor}"
 mongo --host mongodb-dev.snehithdops.online </app/schema/$component.js &>>/tmp/roboshop.log
 systemctl restart $component >>/tmp/roboshop.log
