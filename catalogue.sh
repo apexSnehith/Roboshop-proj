c1e1063afc2826aa95cb4f57d2cc304fa203e5ef
apexSnehith <snehith0713@gmail.com>
2023-05-31 22:43:37 +0530

    creating functions

--- catalogue.sh
+++ catalogue.sh
@@ -3,9 +3,4 @@ component=catalogue
 
 nodejs
 
-echo -e "${color}Install Mongodb client${nocolor}"
-cp /home/centos/Roboshop-proj/mongo.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
-yum install mongodb-org-shell -y &>>${log_file}
-echo -e "${color}load schema${nocolor}"
-mongo --host mongodb-dev.snehithdops.online <${app_path}/schema/$component.js &>>${log_file}
-systemctl restart $component >>${log_file}
+mongo_schema_setup
